cd0bf316054ee3740ba3c5b54479b9657c8e0aee
Đồng Đức <32560999+dongduc@users.noreply.github.com>
2019-03-27 17:17:12 +0700

    Update Excercise

---
+++ FAmail/src/app/models/message.ts
@@ -0,0 +1,11 @@
+export interface Message {
+    folder: string;
+    body: string;
+    subject: string;
+    from: string;
+    to: string;
+    date: string;
+    senderName: [];
+    corpus: string;
+    _id: string;
+}
