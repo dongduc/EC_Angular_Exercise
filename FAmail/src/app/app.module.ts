675a69d633c2706c271e94767a45aef2578a14a6
Đồng Đức <32560999+dongduc@users.noreply.github.com>
2019-03-27 16:38:25 +0700

    Update Excercise

--- FAmail/src/app/app.module.ts
+++ FAmail/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewcmpComponent } from './newcmp/newcmp.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NewcmpComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
