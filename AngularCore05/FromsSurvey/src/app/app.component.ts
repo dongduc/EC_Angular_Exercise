9ee9d1c6f4d4f8cf378bdf676fd6e57faa08d89d
Đồng Đức <32560999+dongduc@users.noreply.github.com>
2019-04-01 14:42:16 +0700

    Update exercise

--- AngularCore05/FromsSurvey/src/app/app.component.ts
+++ AngularCore05/FromsSurvey/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FromsSurvey';
+  
+  onClickSubmit(data) {
+    alert("Survey name: " + data.name);
+  }
 }
